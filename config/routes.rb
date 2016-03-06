3ae350a8021e835aff6642bb9708b848fb0e5eae
Veerand <s.neljandik@gmail.com>
2016-03-06 21:47:28 +0200

    Esialgne andmebaas+routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   
-  get '/proto', :to => redirect('/proto.html')
+  get '/proto', :to => redirect('/avaleht.html')
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
